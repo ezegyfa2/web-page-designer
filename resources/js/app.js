164a9159127a2283b060a70efbfd00a3883a9fb8
Darvas Istvan <darvasvok@gmail.com>
2023-03-25 20:34:47 +0200

    Adding ecom vue components to package includes

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ window.Vue = require('vue').default;
 
 require('dynamic-web-vue-components').default.install(Vue)
 require('blue-admin-vue-components').default.install(Vue)
+require('ecom-vue-components').default.install(Vue)
 
 require('web-page-designer-vue-components').default.install(Vue)
 require('jquery.redirect')
